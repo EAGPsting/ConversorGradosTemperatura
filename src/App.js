82d1d7f5dbf85bd4dedca25f9251f98a6f190114
EAGPsting <93798048+EAGPsting@users.noreply.github.com>
2022-08-15 15:57:01 -0700

    Update App.js
    Tarea 2, conversor de unidades de temperatura

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
+
 function App() {
+  const [Celsius, setCelsius] = useState(0);
+  const [Fahrenheit, setFahrenheit] = useState(0);
+  const [Kelvin, setKelvin] = useState(0);
+
+  const [grado, setGrado] = useState(0);
+  const [tipo, setTipo] = useState(0);
+
+  useEffect(() => {
+    //La data viene en Celsius
+      if (tipo==1)
+      {
+        setFahrenheit((grado*9/5 )+32);
+        setKelvin(((grado*1) + 273.15));
+        setCelsius(grado);
+      } 
+      //La data viene en Fahrenheit
+      if (tipo==2)
+      {
+        setFahrenheit(grado);
+        setKelvin(((grado*1-32)*(5/9))+273.15);
+        setCelsius((grado*1-32)*(5/9));
+      } 
+      //La data viene en Kelvin
+      if (tipo==3)
+      {
+        setFahrenheit(((grado*1-273.15)*(9/5))+32);
+        setKelvin(grado);
+        setCelsius(grado*1-273.15);
+      } 
+
+  }, [grado, tipo]);
+
+
+  const handleTotalChange = e => {
+    if (!isNaN(e.target.value)) {
+      setGrado(e.target.value);
+    }
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Convertidor de unidades de temperatura</h1>
+
+      <p>Fahrenheit: {Fahrenheit}</p>
+      <p>Kelvin: {Kelvin}</p>
+      <p>Celsius: {Celsius}</p>
+      <hr />
+
+      <h3>{grado}</h3>
+
+      <select onChange={event => setTipo(event.target.value)} value={tipo}>
+        <option value={1}>Celsius</option>
+        <option value={2}>Fahrenheit</option>
+        <option value={3}>Kelvin</option>
+      </select>
+      <input onChange={handleTotalChange} value={grado} />
     </div>
   );
 }
